144bb77e2274e0d48542334029e22c165d32cddd
mawar12 <33439852+mawar12@users.noreply.github.com>
2017-11-30 02:25:33 -0500

    Snake eats food.
    When the snake eats the food, the food disappears. Another piece of food is generated at another location.

--- snake.py
+++ snake.py
@@ -71,6 +71,10 @@ def snake_ate_food(snake, food):
     food - 2-tuple representing the position in the grid of the food
     This function should return True if the head of the snake is in the same position as food.
     """
+    if snake[0] == food:
+        food = None
+        while food == None:
+            return True
     return False
 
 def snake_ran_out_of_bounds(snake):
